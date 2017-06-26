15325e14cec22a44f981eeac2fa12d5e1ee58216
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-06-26 13:17:22 +0100

    Initial commit

---
+++ ExampleDataContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Services;
+
+namespace ConsoleDataService
+{
+	public class Person
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class ExampleDataContext
+	{
+		public IQueryable<Person> People
+		{
+			get
+			{
+				return new List<Person>()
+				{
+					new Person() { ID = 1, Name = "Steve"},
+					new Person() { ID = 2, Name = "Dave"}
+				}.AsQueryable();
+			}
+		}		
+	}
+
+	public class PersonDataService : DataService<ExampleDataContext>
+	{
+		public static void InitializeService(DataServiceConfiguration config)
+		{
+			config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Services;
+
+namespace ConsoleDataService
+{
+	public class Program
+	{
+
+		public static void Main()
+		{
+			DataServiceHost host = new DataServiceHost(typeof(PersonDataService), new Uri [] { new Uri("http://localhost:999")});
+
+			try
+			{
+				host.Open();
+
+				Console.WriteLine("Host is running; press a key to stop");
+				Console.ReadKey();
+
+				host.Close();
+			}
+			catch (Exception)
+			{
+				host.Abort();
+				throw;
+			}
+
+		}
+
+	}
+}
